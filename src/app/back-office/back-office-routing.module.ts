01a409fd831d9022c6fc759c97fe1d5edd137472
Kheireddine Mechergui <kheireddine.mechergui@esprit.tn>
2022-11-09 06:15:59 -0800

    refactor (struct) updating global project struct

--- src/app/back-office/back-office-routing.module.ts
+++ src/app/back-office/back-office-routing.module.ts
@@ -1,10 +1,33 @@
-import { GestionDonsComponent } from './gestion-dons/gestion-dons.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BackOfficeComponent } from './back-office.component';
 
-const routes: Routes = [{ path: '', component: BackOfficeComponent , 
-children:[{path: 'gestiondons',component:GestionDonsComponent}]},
+import { GestionDonsComponent } from './gestion-dons/gestion-dons.component';
+import {NotFoundComponent} from "../shared/not-found/not-found.component";
+import {AdminUserManagementComponent} from "./components/admin-user-management/admin-user-management.component";
+import {AdminGuestContactComponent} from "./components/admin-guest-contact/admin-guest-contact.component";
+import {AdminDashbordComponent} from "./components/admin-dashbord/admin-dashbord.component";
+import {AdminGuard} from "../core/guards/admin.guard";
+import {BenevoleGuard} from "../core/guards/benevole.guard";
+import {DoctorGuard} from "../core/guards/doctor.guard";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BackOfficeComponent ,
+    children:[
+      {path: 'gestiondons',component:GestionDonsComponent},
+
+      // Start Admin Routes Section
+      {path: 'admin/dashboard',component:AdminDashbordComponent, canActivate: [/*AdminGuard*/]},
+      {path: 'admin/users',component:AdminUserManagementComponent, canActivate: [/*AdminGuard*/]},
+      {path: 'admin/contact',component:AdminGuestContactComponent, canActivate: [/*AdminGuard*/]},
+      {path: 'admin', redirectTo: 'admin/dashboard', pathMatch: 'full'},
+      // End Admin Routes Section
+
+      {path: '**', component: NotFoundComponent}
+    ]
+  },
 ];
 
 @NgModule({
--- src/app/back-office/back-office.module.ts
+++ src/app/back-office/back-office.module.ts
@@ -1,21 +1,49 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { BackOfficeRoutingModule } from './back-office-routing.module';
-import { BackOfficeComponent } from './back-office.component';
-import { GestionDonsComponent } from './gestion-dons/gestion-dons.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
+import { BackOfficeComponent } from "./back-office.component";
+import { BackOfficeMaterialModule } from "../core/material/back-office-material.module";
+
+
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {SideBarComponent} from "./side-bar/side-bar.component";
+
+
+import { AdminDashbordComponent } from './components/admin-dashbord/admin-dashbord.component';
+import { AdminUserManagementComponent } from './components/admin-user-management/admin-user-management.component';
+import { AdminGuestContactComponent } from './components/admin-guest-contact/admin-guest-contact.component';
+import { AdminEditUsersDialogComponent } from './components/dialogs/admin-edit-users-dialog/admin-edit-users-dialog.component';
+import { AdminDashboardUserStatisticsComponent } from './components/admin-dashboard-user-statistics/admin-dashboard-user-statistics.component';
+import { AdminConfirmDeleteUserDialogComponent } from './components/dialogs/admin-confirm-delete-user-dialog/admin-confirm-delete-user-dialog.component';
+import { AdminUserDetailsDialogComponent } from './components/dialogs/admin-user-details-dialog/admin-user-details-dialog.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatRadioModule} from "@angular/material/radio";
+import {MatSelectModule} from "@angular/material/select";
+import {MatChipsModule} from "@angular/material/chips";
+import {NgxChartsModule} from "@swimlane/ngx-charts";
 
 
 @NgModule({
   declarations: [
     BackOfficeComponent,
-    GestionDonsComponent,
-    SideBarComponent
+    SideBarComponent,
+    AdminDashbordComponent,
+    AdminUserManagementComponent,
+    AdminGuestContactComponent,
+    AdminEditUsersDialogComponent,
+    AdminDashboardUserStatisticsComponent,
+    AdminConfirmDeleteUserDialogComponent,
+    AdminUserDetailsDialogComponent
   ],
-  imports: [
-    CommonModule,
-    BackOfficeRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        BackOfficeRoutingModule,
+        BackOfficeMaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NgxChartsModule,
+        MatChipsModule
+    ]
 })
 export class BackOfficeModule { }
