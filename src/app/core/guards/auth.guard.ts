16ce033bb9b7850a1d681f07ccadac630a481378
Kheireddine Mechergui <kheireddine.mechergui@esprit.tn>
2022-11-09 05:59:53 -0800

    feat (guard) Adding AuthGuard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateChild, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivateChild, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AccountService} from "../services/account.service";
+import {RoleEnum} from "../entities/users";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivateChild {
-  constructor(private _accountService:AccountService) {}
+  constructor(private _accountService:AccountService,private _router: Router) {}
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this._accountService.isAuthenticated();
+    if(this._accountService.isAuthenticated()) {
+      return true;
+    }
+    this._router.navigateByUrl("/FrontOffice/login");
+    return false;
   }
 
 }
