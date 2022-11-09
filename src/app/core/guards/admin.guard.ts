b711a2e3b26ef6fb379de0080b19a6067b756c96
Kheireddine Mechergui <kheireddine.mechergui@esprit.tn>
2022-11-09 06:02:14 -0800

    feat (guard) Adding Guards, to limit Access By Role

--- src/app/core/guards/admin.guard.ts
+++ src/app/core/guards/admin.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AccountService} from "../services/account.service";
 import {RoleEnum} from "../entities/users";
@@ -8,11 +8,16 @@ import {RoleEnum} from "../entities/users";
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
-  constructor(private _accountService:AccountService) {}
+  constructor(private _accountService:AccountService,private _router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this._accountService.hasRole(RoleEnum.ADMIN);
+    if(this._accountService.hasRole(RoleEnum.ADMIN)) {
+      return true;
+    }
+    this._accountService.logOut();
+    this._router.navigateByUrl("/FrontOffice/login");
+    return false;
   }
 
 }
--- src/app/core/guards/benefecier.guard.ts
+++ src/app/core/guards/benefecier.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
 import {RoleEnum} from "../entities/users";
 import {AccountService} from "../services/account.service";
@@ -8,11 +8,16 @@ import {AccountService} from "../services/account.service";
   providedIn: 'root'
 })
 export class BenefecierGuard implements CanActivate {
-  constructor(private _accountService:AccountService) {}
+  constructor(private _accountService:AccountService,private _router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this._accountService.hasRole(RoleEnum.BENEFICIER);
+    if(this._accountService.hasRole(RoleEnum.BENEFICIER)) {
+      return true;
+    }
+    this._accountService.logOut();
+    this._router.navigateByUrl("/FrontOffice/login");
+    return false;
   }
 
 }
