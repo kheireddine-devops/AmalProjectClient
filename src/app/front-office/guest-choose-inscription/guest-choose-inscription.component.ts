30a3cb1cb5d1770b6ebfdcaf6c6043e46fa7a83c
Kheireddine Mechergui <kheireddine.mechergui@esprit.tn>
2022-11-09 05:55:59 -0800

    feat (views) Adding View ChooseInscription

---
+++ src/app/front-office/guest-choose-inscription/guest-choose-inscription.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-choose-inscription',
+  templateUrl: './guest-choose-inscription.component.html',
+  styleUrls: ['./guest-choose-inscription.component.css']
+})
+export class GuestChooseInscriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
