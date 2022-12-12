651128d77e9cb531fab25de58e3e90638e3e458f
hasnisabrine <sabrinehasni3@gmail.com>
2022-12-12 20:15:34 +0100

    desactivate postuler button to guest

--- src/app/front-office/tous-emplois/tous-emplois.component.ts
+++ src/app/front-office/tous-emplois/tous-emplois.component.ts
@@ -5,6 +5,8 @@ import { Emploi } from 'src/app/core/entities/Emlpoi';
 import { first } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { EmploiDetailPopupComponent } from '../emploi-detail-popup/emploi-detail-popup.component';
+import { AccountService } from 'src/app/core/services/account.service';
+import { RoleEnum } from 'src/app/core/entities/users';
 @Component({
   selector: 'app-tous-emplois',
   templateUrl: './tous-emplois.component.html',
@@ -15,10 +17,13 @@ export class TousEmploisComponent implements OnInit {
   compte:any;
   p:number=1;
   secteur:any;
-  constructor(private dataTransferService:DataTransferService ,private dialogRef :MatDialog,private emploiService: EmploiService) { }
+  role: RoleEnum | undefined;
+  isBenificier:boolean =false;
+  constructor(private accountService: AccountService,private dataTransferService:DataTransferService ,private dialogRef :MatDialog,private emploiService: EmploiService) { }
   ngOnInit(): void {
+    this.role = this.accountService.getRole();
+    if (this.role== RoleEnum.BENEFICIER){this.isBenificier=true}
     this.emploiService.getEmploi().subscribe(()=> this.emploiService.getEmploi().pipe(first()).subscribe((data)=>this.tousEmplois=data))
-   
     console.log("list emplois" +this.tousEmplois)
   }
   onPostuler(e: Emploi){
